873642d38a3509949a2678c76248cdc60ebea451
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-17 16:16:50 +0100

    feat(block): initiate iblock class

---
+++ src/test/kotlin/IBlockTest.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class IBlockTest {
+
+    @Test
+    fun `position 0,0 needed positions are (0,0), (0,1), (0,2), (0,3)`() {
+        val iBlock: IBlock = IBlock(Position(0, 0))
+        assertEquals(
+            listOf(Position(0, 0), Position(0, 1), Position(0, 2), Position(0, 3)),
+            iBlock.getNeededPositions()
+        )
+    }
+
+}
