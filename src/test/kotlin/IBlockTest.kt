7659607572465a928c076f8ee052823ecd7522f9
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-17 16:22:05 +0100

    [feature]: Add IBlock Needed positions for variable y axis

--- src/test/kotlin/IBlockTest.kt
+++ src/test/kotlin/IBlockTest.kt
@@ -12,4 +12,13 @@ class IBlockTest {
         )
     }
 
+    @Test
+    fun `position 0,1 needed positions are (0,1), (0,2), (0,3), (0,4)`() {
+        val iBlock: IBlock = IBlock(Position(0, 1))
+        assertEquals(
+            listOf(Position(0, 1), Position(0, 2), Position(0, 3), Position(0, 4)),
+            iBlock.getNeededPositions()
+        )
+    }
+
 }
