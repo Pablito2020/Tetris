ccc16588e69857d3ee552c47549522ebdd106347
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-17 17:32:23 +0100

    test(block): reorganized structure

--- src/test/kotlin/blocks/IBlockSimpleRotationsTest.kt
+++ src/test/kotlin/blocks/IBlockSimpleRotationsTest.kt
@@ -1,44 +1,15 @@
-import blocks.IBlock
+package blocks
+
+import blocks.interfaces.SimpleRotation
 import movements.Position
 import movements.Rotation
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
-class IBlockTest {
-
-    // Trivial Positions
+class IBlockSimpleRotationsTest : SimpleRotation {
 
     @Test
-    fun `position 0,0 needed positions are (1,0), (1,1), (1,2), (1,3)`() {
-        val iBlock = IBlock(Position(0, 0))
-        assertEquals(
-            listOf(Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)),
-            iBlock.getNeededPositions()
-        )
-    }
-
-    @Test
-    fun `position 0,1 needed positions are (1,1), (1,2), (1,3), (1,4)`() {
-        val iBlock = IBlock(Position(0, 1))
-        assertEquals(
-            listOf(Position(1, 1), Position(1, 2), Position(1, 3), Position(1, 4)),
-            iBlock.getNeededPositions()
-        )
-    }
-
-    @Test
-    fun `position 1,0 needed positions are (2,0), (2,1), (2,2), (2,3)`() {
-        val iBlock = IBlock(Position(1, 0))
-        assertEquals(
-            listOf(Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)),
-            iBlock.getNeededPositions()
-        )
-    }
-
-    // Rotate 90 degrees to Right
-
-    @Test
-    fun `Rotate block 90 degrees to right changed needed positions from (0,0)`() {
+    override fun `Rotate block 90 degrees to right changed needed positions from (0,0)`() {
         val iBlock = IBlock(Position(0, 0))
         iBlock.rotate(Rotation.RIGHT_90_DEGREE)
         assertEquals(
@@ -48,7 +19,7 @@ class IBlockTest {
     }
 
     @Test
-    fun `Rotate block 90 degrees to right changed needed positions from (0, 1)`() {
+    override fun `Rotate block 90 degrees to right changed needed positions from (0, 1)`() {
         val iBlock = IBlock(Position(0, 1))
         iBlock.rotate(Rotation.RIGHT_90_DEGREE)
         assertEquals(
@@ -58,7 +29,7 @@ class IBlockTest {
     }
 
     @Test
-    fun `Rotate block 90 degrees to right changed needed positions from (1, 0)`() {
+    override fun `Rotate block 90 degrees to right changed needed positions from (1, 0)`() {
         val iBlock = IBlock(Position(1, 0))
         iBlock.rotate(Rotation.RIGHT_90_DEGREE)
         assertEquals(
@@ -67,10 +38,8 @@ class IBlockTest {
         )
     }
 
-    // Rotate 90 degrees to Left
-
     @Test
-    fun `Rotate 90 Degrees to Left changes Position from 0,0`() {
+    override fun `Rotate 90 Degrees to Left changes Position from 0,0`() {
         val iBlock = IBlock(Position(0, 0))
         iBlock.rotate(Rotation.LEFT_90_DEGREE)
         assertEquals(
@@ -80,7 +49,7 @@ class IBlockTest {
     }
 
     @Test
-    fun `Rotate block 90 degrees to Left changed needed positions from (0, 1)`() {
+    override fun `Rotate block 90 degrees to Left changed needed positions from (0, 1)`() {
         val iBlock = IBlock(Position(0, 1))
         iBlock.rotate(Rotation.LEFT_90_DEGREE)
         assertEquals(
@@ -90,7 +59,7 @@ class IBlockTest {
     }
 
     @Test
-    fun `Rotate block 90 degrees to Left changed needed positions from (1, 0)`() {
+    override fun `Rotate block 90 degrees to Left changed needed positions from (1, 0)`() {
         val iBlock = IBlock(Position(1, 0))
         iBlock.rotate(Rotation.LEFT_90_DEGREE)
         assertEquals(
@@ -98,5 +67,6 @@ class IBlockTest {
             iBlock.getNeededPositions()
         )
     }
+
 
 }
