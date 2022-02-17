15203f19696d2848878cb13d69bc9ca1adc95fe0
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-17 17:17:11 +0100

    [Refactor]: IBlock better naming

--- src/main/kotlin/IBlock.kt
+++ src/main/kotlin/IBlock.kt
@@ -3,9 +3,9 @@ import Rotation.RIGHT_90_DEGREE
 
 class IBlock(private val initialPosition: Position) {
 
-    private var current_type_iblock_index = 0
+    private var currentTypeIBlockIndex = 0
 
-    private val IBLOCK_POSSIBLE_VALUES = listOf(
+    private val TYPES = listOf(
         listOf(Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)),
         listOf(Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)),
         listOf(Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)),
@@ -14,16 +14,16 @@ class IBlock(private val initialPosition: Position) {
 
     fun getNeededPositions(): Collection<Position> {
         val result = ArrayList<Position>()
-        for (currentPosition in IBLOCK_POSSIBLE_VALUES[current_type_iblock_index])
+        for (currentPosition in TYPES[currentTypeIBlockIndex])
             result.add(Position(currentPosition.x + initialPosition.x, currentPosition.y + initialPosition.y))
         return result
     }
 
     fun rotate(degree: Rotation) {
-        current_type_iblock_index = when (degree) {
-            RIGHT_90_DEGREE -> current_type_iblock_index + 1 % IBLOCK_POSSIBLE_VALUES.size
+        currentTypeIBlockIndex = when (degree) {
+            RIGHT_90_DEGREE -> currentTypeIBlockIndex + 1 % TYPES.size
             LEFT_90_DEGREE -> {
-                if (current_type_iblock_index == 0) IBLOCK_POSSIBLE_VALUES.size - 1 else current_type_iblock_index - 1
+                if (currentTypeIBlockIndex == 0) TYPES.size - 1 else currentTypeIBlockIndex - 1
             }
         }
     }
