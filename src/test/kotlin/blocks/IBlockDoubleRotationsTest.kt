245b1cb5d154eee96dd54c4c37b2a035714ecacf
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-17 17:51:35 +0100

    test(iblock): double left/right rotation cases

--- src/test/kotlin/blocks/IBlockDoubleRotationsTest.kt
+++ src/test/kotlin/blocks/IBlockDoubleRotationsTest.kt
@@ -28,4 +28,26 @@ class IBlockDoubleRotationsTest {
             iBlock.getNeededPositions()
         )
     }
+
+    @Test
+    fun `Rotate block 180 degrees to left`() {
+        val iBlock = IBlock(Position(0, 0))
+        iBlock.rotate(Rotation.LEFT_90_DEGREE)
+        iBlock.rotate(Rotation.LEFT_90_DEGREE)
+        assertEquals(
+            listOf(Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)),
+            iBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    fun `Rotate block 180 degrees to right`() {
+        val iBlock = IBlock(Position(0, 0))
+        iBlock.rotate(Rotation.RIGHT_90_DEGREE)
+        iBlock.rotate(Rotation.RIGHT_90_DEGREE)
+        assertEquals(
+            listOf(Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)),
+            iBlock.getNeededPositions()
+        )
+    }
 }
