4a96c499fa8e0329656947d02174966e0acfe756
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-17 16:59:09 +0100

    test(iblock): add rotation 90 Degree to Right case

--- src/test/kotlin/IBlockTest.kt
+++ src/test/kotlin/IBlockTest.kt
@@ -3,6 +3,8 @@ import kotlin.test.assertEquals
 
 class IBlockTest {
 
+    // Trivial Positions
+
     @Test
     fun `position 0,0 needed positions are (1,0), (1,1), (1,2), (1,3)`() {
         val iBlock = IBlock(Position(0, 0))
@@ -30,12 +32,34 @@ class IBlockTest {
         )
     }
 
+    // Rotate 90 degrees
+
     @Test
-    fun `Rotate block 90 degrees to right changed needed positions`() {
+    fun `Rotate block 90 degrees to right changed needed positions from (0,0)`() {
         val iBlock = IBlock(Position(0, 0))
         iBlock.rotate(Rotation.RIGHT_90_DEGREE)
         assertEquals(
             listOf(Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)),
+            iBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    fun `Rotate block 90 degrees to right changed needed positions from (0, 1)`() {
+        val iBlock = IBlock(Position(0, 1))
+        iBlock.rotate(Rotation.RIGHT_90_DEGREE)
+        assertEquals(
+            listOf(Position(0, 3), Position(1, 3), Position(2, 3), Position(3, 3)),
+            iBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    fun `Rotate block 90 degrees to right changed needed positions from (1, 0)`() {
+        val iBlock = IBlock(Position(1, 0))
+        iBlock.rotate(Rotation.RIGHT_90_DEGREE)
+        assertEquals(
+            listOf(Position(1, 2), Position(2, 2), Position(3, 2), Position(4, 2)),
             iBlock.getNeededPositions()
         )
     }
