cf143ed23c15cace58c84d883fdfff1e08232fd2
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-17 19:48:45 +0100

    feat(position): implement addAxes

--- src/main/kotlin/movements/Position.kt
+++ src/main/kotlin/movements/Position.kt
@@ -3,11 +3,13 @@ package movements
 data class Position(val row: Int, val column: Int) {
 
     fun move(direction: Direction): Position {
-        return when(direction) {
+        return when (direction) {
             Direction.DOWN -> Position(row + 1, column)
             Direction.LEFT -> Position(row, column - 1)
-            Direction.RIGHT -> Position(row , column + 1)
+            Direction.RIGHT -> Position(row, column + 1)
         }
     }
+
+    fun addAxes(other: Position): Position = Position(row + other.row, column + other.column)
 
 }
--- src/test/kotlin/movements/PositionTest.kt
+++ src/test/kotlin/movements/PositionTest.kt
@@ -28,4 +28,28 @@ class PositionTest {
         assertEquals(Position(1,2), position.move(Direction.RIGHT))
     }
 
+    @Test
+    fun `Add axes with (0, 0) position`(){
+        val other = Position(0, 0)
+        assertEquals(Position(1, 1), position.addAxes(other))
+    }
+
+    @Test
+    fun `Add axes with negative row position`(){
+        val other = Position(-1, 0)
+        assertEquals(Position(0, 1), position.addAxes(other))
+    }
+
+    @Test
+    fun `Add axes with negative column position`(){
+        val other = Position(0, -1)
+        assertEquals(Position(1, 0), position.addAxes(other))
+    }
+
+    @Test
+    fun `Simple positive add`(){
+        val other = Position(1, 1)
+        assertEquals(Position(2, 2), position.addAxes(other))
+    }
+
 }
