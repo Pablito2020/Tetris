425ffba042a14181511af15280f8f6b506949421
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-17 17:32:23 +0100

    [Refactor]: Reorganized test structure

---
+++ src/test/kotlin/blocks/IBlockTest.kt
@@ -0,0 +1,38 @@
+package blocks
+
+import movements.Position
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class IBlockTest {
+
+    // Trivial Positions
+
+    @Test
+    fun `position 0,0 needed positions are (1,0), (1,1), (1,2), (1,3)`() {
+        val iBlock = IBlock(Position(0, 0))
+        assertEquals(
+            listOf(Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)),
+            iBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    fun `position 0,1 needed positions are (1,1), (1,2), (1,3), (1,4)`() {
+        val iBlock = IBlock(Position(0, 1))
+        assertEquals(
+            listOf(Position(1, 1), Position(1, 2), Position(1, 3), Position(1, 4)),
+            iBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    fun `position 1,0 needed positions are (2,0), (2,1), (2,2), (2,3)`() {
+        val iBlock = IBlock(Position(1, 0))
+        assertEquals(
+            listOf(Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)),
+            iBlock.getNeededPositions()
+        )
+    }
+
+}
---
+++ src/test/kotlin/blocks/interfaces/SimpleRotation.kt
@@ -0,0 +1,29 @@
+package blocks.interfaces
+
+import org.junit.jupiter.api.Test
+
+interface SimpleRotation {
+
+    // Rotate to Right
+
+    @Test
+    fun `Rotate block 90 degrees to right changed needed positions from (0,0)`()
+
+    @Test
+    fun `Rotate block 90 degrees to right changed needed positions from (0, 1)`()
+
+    @Test
+    fun `Rotate block 90 degrees to right changed needed positions from (1, 0)`()
+
+    // Rotate to Left
+
+    @Test
+    fun `Rotate 90 Degrees to Left changes Position from 0,0`()
+
+    @Test
+    fun `Rotate block 90 degrees to Left changed needed positions from (0, 1)`()
+
+    @Test
+    fun `Rotate block 90 degrees to Left changed needed positions from (1, 0)`()
+
+}
