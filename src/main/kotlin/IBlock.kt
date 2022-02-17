a310f485f281e6034a51c31b674de5579c65c52b
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-17 16:28:23 +0100

    [Feature]: IBlock implement x movement

--- src/main/kotlin/IBlock.kt
+++ src/main/kotlin/IBlock.kt
@@ -3,7 +3,7 @@ class IBlock(val position: Position) {
     fun getNeededPositions(): Collection<Position> {
         val result = ArrayList<Position>()
         for (i in 0..3)
-            result.add(Position(0, this.position.y + i))
+            result.add(Position(position.x, this.position.y + i))
         return result
     }
 
