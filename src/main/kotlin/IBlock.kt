9169c36ce0e31e807c52b1d1efbfdd916fc5a248
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-17 16:18:34 +0100

    [feature] block: implement initial position

--- src/main/kotlin/IBlock.kt
+++ src/main/kotlin/IBlock.kt
@@ -1,7 +1,7 @@
 class IBlock(position: Position) {
 
     fun getNeededPositions(): List<Position> {
-        TODO()
+        return listOf(Position(0,0), Position(0, 1), Position(0, 2), Position(0, 3))
     }
 
 }
