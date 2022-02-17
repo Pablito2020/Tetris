c35669e15efc1dd3540bcbc3e1bb9a9f56964031
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-17 18:48:43 +0100

    [Refactor] Abstraction for testing the move method

--- src/test/kotlin/blocks/IBlockTest.kt
+++ src/test/kotlin/blocks/IBlockTest.kt
@@ -6,8 +6,6 @@ import kotlin.test.assertEquals
 
 class IBlockTest {
 
-    // Trivial Positions
-
     @Test
     fun `position 0,0 needed positions are (1,0), (1,1), (1,2), (1,3)`() {
         val iBlock = IBlock(Position(0, 0))
