409aa111d859c139aa881ec2829111e5623dde5a
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-17 18:17:56 +0100

    [Test]: IBlock check for double move cases

--- src/test/kotlin/blocks/IBlockMoveTest.kt
+++ src/test/kotlin/blocks/IBlockMoveTest.kt
@@ -12,14 +12,14 @@ class IBlockMoveTest {
 
     @BeforeEach
     fun setUp() {
-        this.IBlock = IBlock(Position(1, 1))
+        this.IBlock = IBlock(Position(2, 2))
     }
 
     @Test
     fun `Move to Right changes position plus one`() {
         IBlock.move(Direction.LEFT)
         assertEquals(
-            listOf(Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)),
+            listOf(Position(3, 1), Position(3, 2), Position(3, 3), Position(3, 4)),
             IBlock.getNeededPositions()
         )
     }
@@ -28,10 +28,29 @@ class IBlockMoveTest {
     fun `Move to Left changes position plus one`() {
         IBlock.move(Direction.RIGHT)
         assertEquals(
-            listOf(Position(2, 2), Position(2, 3), Position(2, 4), Position(2, 5)),
+            listOf(Position(3, 3), Position(3, 4), Position(3, 5), Position(3, 6)),
             IBlock.getNeededPositions()
         )
+    }
 
+    @Test
+    fun `Double move to Left changes position plus two`() {
+        IBlock.move(Direction.LEFT)
+        IBlock.move(Direction.LEFT)
+        assertEquals(
+            listOf(Position(3, 0), Position(3, 1), Position(3, 2), Position(3, 3)),
+            IBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    fun `Double move to Right changes position plus two`() {
+        IBlock.move(Direction.RIGHT)
+        IBlock.move(Direction.RIGHT)
+        assertEquals(
+            listOf(Position(3, 4), Position(3, 5), Position(3, 6), Position(3, 7)),
+            IBlock.getNeededPositions()
+        )
     }
 
 }
