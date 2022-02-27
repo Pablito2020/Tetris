e370a691a9f95678d8af7eccfff7b36c5a7eee8c
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-27 16:33:16 +0100

    feat(sblock): simple rotation

--- src/main/kotlin/blocks/implementation/SBlock.kt
+++ src/main/kotlin/blocks/implementation/SBlock.kt
@@ -8,13 +8,19 @@ import movements.Position
 class SBlock(private var position: Position) : Block {
 
     private val positions = listOf(Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1))
+    private val rightDegreePositions = listOf(Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2))
+    private val leftDegreePositions = listOf(Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1))
+    private var currentPosition: List<Position> = positions
 
-    override fun getNeededPositions(): Collection<Position> = positions.map { p ->
+    override fun getNeededPositions(): Collection<Position> = currentPosition.map { p ->
         Position(p.row + position.row, p.column + position.column)
     }
 
     override fun rotate(degree: Rotation) {
-        TODO("Not yet implemented")
+        currentPosition = when(degree) {
+            Rotation.RIGHT_90_DEGREE -> rightDegreePositions
+            Rotation.LEFT_90_DEGREE -> leftDegreePositions
+        }
     }
 
     override fun move(direction: Direction) {
