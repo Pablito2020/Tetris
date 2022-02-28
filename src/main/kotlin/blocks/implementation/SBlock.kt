1d8fcbebc9f5771c7143e89149a4fe2b1eaf399c
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-28 16:45:20 +0100

    refactor(sblock): use delegation, no inheritance

--- src/main/kotlin/blocks/implementation/SBlock.kt
+++ src/main/kotlin/blocks/implementation/SBlock.kt
@@ -5,29 +5,21 @@ import blocks.Rotation
 import movements.Direction
 import movements.Position
 
-class SBlock(private var position: Position) : Block {
+internal val POSITIONS = listOf(
+    listOf(Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1)),
+    listOf(Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2)),
+    listOf(Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1)),
+    listOf(Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1))
+)
 
-    private val POSITIONS = listOf(
-        listOf(Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1)),
-        listOf(Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1)),
-        listOf(Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1)),
-        listOf(Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2))
-    )
-    private var positionIndex = 0
+class SBlock(position: Position) : Block {
 
-    override fun getNeededPositions(): Collection<Position> = POSITIONS[positionIndex].map { p ->
-        Position(p.row + position.row, p.column + position.column)
-    }
+    private val blockLogic = BlockLogic(POSITIONS, position)
 
-    override fun rotate(degree: Rotation) {
-        positionIndex = when (degree) {
-            Rotation.RIGHT_90_DEGREE -> if (positionIndex - 1 < 0) POSITIONS.size - 1 else positionIndex - 1
-            Rotation.LEFT_90_DEGREE -> (positionIndex + 1) % POSITIONS.size
-        }
-    }
+    override fun getNeededPositions(): Collection<Position> = blockLogic.getNeededPositions()
 
-    override fun move(direction: Direction) {
-        position = position.move(direction)
-    }
+    override fun rotate(degree: Rotation) = blockLogic.rotate(degree)
+
+    override fun move(direction: Direction) = blockLogic.move(direction)
 
 }
