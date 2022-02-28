be7b678655b98fdc5ad9a6ab62b213cdf742d113
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-02-28 17:29:14 +0100

    refactor(squareblock): use common logic

--- src/main/kotlin/blocks/implementation/SquareBlock.kt
+++ src/main/kotlin/blocks/implementation/SquareBlock.kt
@@ -5,20 +5,18 @@ import blocks.Rotation
 import movements.Direction
 import movements.Position
 
-class SquareBlock(private var initialPosition: Position) : Block {
+internal val positions = listOf(
+    Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)
+)
 
-    private val positions = listOf(
-        Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)
-    )
+class SquareBlock(initialPosition: Position) : Block {
 
-    override fun getNeededPositions(): Collection<Position> {
-        return positions.map { position -> position.addAxes(initialPosition) }
-    }
+    private val logic = BlockLogic(listOf(positions), initialPosition)
+
+    override fun getNeededPositions(): Collection<Position> = logic.getNeededPositions()
 
     override fun rotate(degree: Rotation) {}
 
-    override fun move(direction: Direction) {
-        initialPosition = initialPosition.move(direction)
-    }
+    override fun move(direction: Direction) = logic.move(direction)
 
 }
