7aabcb7873ef74f2ad1cabd488b802937827a472
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-01 15:47:32 +0100

    feat(zblock): simple rotation

--- src/main/kotlin/blocks/implementation/ZBlock.kt
+++ src/main/kotlin/blocks/implementation/ZBlock.kt
@@ -7,14 +7,19 @@ import movements.Position
 
 class ZBlock(private var initialPosition: Position): Block {
 
-    private val positions = listOf(Position(0,0), Position(0,1), Position(1,1), Position(1,2))
+    private var positions = listOf(Position(0,0), Position(0,1), Position(1,1), Position(1,2))
+    private val rightPositions = listOf(Position(0, 2), Position(1, 1), Position(1, 2), Position(2, 1))
+    private val leftPositions = listOf(Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 0))
 
     override fun getNeededPositions(): Collection<Position> {
         return positions.map { position -> position.addAxes(initialPosition) }
     }
 
     override fun rotate(degree: Rotation) {
-        TODO("Not yet implemented")
+        positions = when(degree)  {
+            Rotation.LEFT_90_DEGREE -> leftPositions
+            Rotation.RIGHT_90_DEGREE -> rightPositions
+        }
     }
 
     override fun move(direction: Direction) {
---
+++ src/test/kotlin/blocks/ZBlock/ZBlockSimpleRotation.kt
@@ -0,0 +1,72 @@
+package blocks.ZBlock
+
+import blocks.Rotation
+import blocks.SimpleRotation
+import blocks.implementation.ZBlock
+import movements.Position
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ZBlockSimpleRotation : SimpleRotation {
+
+    @Test
+    override fun `Rotate block 90 degrees to right changed needed positions from (0,0)`() {
+        val zBlock = ZBlock(Position(0, 0))
+        zBlock.rotate(Rotation.RIGHT_90_DEGREE)
+        assertEquals(
+            listOf(Position(0, 2), Position(1, 1), Position(1, 2), Position(2, 1)),
+            zBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    override fun `Rotate block 90 degrees to right changed needed positions from (0, 1)`() {
+        val zBlock = ZBlock(Position(0, 1))
+        zBlock.rotate(Rotation.RIGHT_90_DEGREE)
+        assertEquals(
+            listOf(Position(0, 3), Position(1, 2), Position(1, 3), Position(2, 2)),
+            zBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    override fun `Rotate block 90 degrees to right changed needed positions from (1, 0)`() {
+        val zBlock = ZBlock(Position(1, 0))
+        zBlock.rotate(Rotation.RIGHT_90_DEGREE)
+        assertEquals(
+            listOf(Position(1, 2), Position(2, 1), Position(2, 2), Position(3, 1)),
+            zBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    override fun `Rotate 90 Degrees to Left changes Position from 0,0`() {
+        val zBlock = ZBlock(Position(0, 0))
+        zBlock.rotate(Rotation.LEFT_90_DEGREE)
+        assertEquals(
+            listOf(Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 0)),
+            zBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    override fun `Rotate block 90 degrees to Left changed needed positions from (0, 1)`() {
+        val zBlock = ZBlock(Position(0, 1))
+        zBlock.rotate(Rotation.LEFT_90_DEGREE)
+        assertEquals(
+            listOf(Position(0, 2), Position(1, 1), Position(1, 2), Position(2, 1)),
+            zBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    override fun `Rotate block 90 degrees to Left changed needed positions from (1, 0)`() {
+        val zBlock = ZBlock(Position(1, 0))
+        zBlock.rotate(Rotation.LEFT_90_DEGREE)
+        assertEquals(
+            listOf(Position(1, 1), Position(2, 0), Position(2, 1), Position(3, 0)),
+            zBlock.getNeededPositions()
+        )
+    }
+
+}
