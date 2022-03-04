85cca95944ffb0a8237c336f127cbdbae413f161
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-04 17:13:25 +0100

    refactor(lblock): delete duplication

--- src/main/kotlin/blocks/implementation/LBlock.kt
+++ src/main/kotlin/blocks/implementation/LBlock.kt
@@ -5,41 +5,21 @@ import blocks.Rotation
 import movements.Direction
 import movements.Position
 
-class LBlock(private var initialPosition: Position): Block {
+internal val lBlockPositions = listOf(
+    listOf(Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)),
+    listOf(Position(0, 1), Position(1, 1), Position(2, 1), Position(2, 2)),
+    listOf(Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)),
+    listOf(Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1))
+)
 
-    val defaultPositions = listOf(Position(0, 2), Position(1, 0), Position(1, 1), Position(1,2))
-    val rightPositions = listOf(Position(0, 1), Position(1, 1), Position(2, 1), Position(2,2))
-    val leftPositions = listOf(Position(0, 0), Position(0, 1), Position(1, 1), Position(2,1))
-    val middlePosition = listOf(Position(1, 0), Position(1, 1), Position(1,2), Position(2, 0))
-    var currentPosition = defaultPositions
+class LBlock(initialPosition: Position) : Block {
 
-    override fun getNeededPositions(): Collection<Position> {
-        return currentPosition.map({pos -> pos.addAxes(initialPosition)})
-    }
+    private val logic = BlockLogic(lBlockPositions, initialPosition)
 
-    override fun rotate(degree: Rotation) {
-        currentPosition = when(degree) {
-            Rotation.RIGHT_90_DEGREE -> {
-                if (currentPosition == leftPositions)
-                    defaultPositions
-                else if (currentPosition == rightPositions)
-                    middlePosition
-                else
-                    rightPositions
-            }
-            Rotation.LEFT_90_DEGREE -> {
-                if (currentPosition == rightPositions)
-                    defaultPositions
-                else if (currentPosition == leftPositions)
-                    middlePosition
-                else
-                    leftPositions
-            }
-        }
-    }
+    override fun getNeededPositions(): Collection<Position> = logic.getNeededPositions()
 
-    override fun move(direction: Direction) {
-        initialPosition = initialPosition.move(direction)
-    }
+    override fun rotate(degree: Rotation) = logic.rotate(degree)
+
+    override fun move(direction: Direction) = logic.move(direction)
 
 }
