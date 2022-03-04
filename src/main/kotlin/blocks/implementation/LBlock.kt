5105eb271eecb6aa2a920c9fe1df1464455f2eb5
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-04 16:50:39 +0100

    feat(lblock): implement simple rotation

--- src/main/kotlin/blocks/implementation/LBlock.kt
+++ src/main/kotlin/blocks/implementation/LBlock.kt
@@ -7,14 +7,19 @@ import movements.Position
 
 class LBlock(private var initialPosition: Position): Block {
 
-    val positions = listOf(Position(0, 0), Position(1, 0), Position(2, 0), Position(2,1))
+    var positions = listOf(Position(0, 0), Position(1, 0), Position(2, 0), Position(2,1))
+    val rightPositions = listOf(Position(0, 1), Position(1, 1), Position(2, 1), Position(2,2))
+    val leftPositions = listOf(Position(0, 0), Position(0, 1), Position(1, 1), Position(2,1))
 
     override fun getNeededPositions(): Collection<Position> {
         return positions.map({pos -> pos.addAxes(initialPosition)})
     }
 
     override fun rotate(degree: Rotation) {
-        TODO("Not yet implemented")
+        positions = when(degree) {
+            Rotation.RIGHT_90_DEGREE -> rightPositions
+            Rotation.LEFT_90_DEGREE -> leftPositions
+        }
     }
 
     override fun move(direction: Direction) {
