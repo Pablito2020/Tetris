8cc4ea3f8a5e98f00b197bd3d49e5fad5e463f7c
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-04 17:11:22 +0100

    feat(lblock): implement double rotation

--- src/main/kotlin/blocks/implementation/LBlock.kt
+++ src/main/kotlin/blocks/implementation/LBlock.kt
@@ -7,18 +7,34 @@ import movements.Position
 
 class LBlock(private var initialPosition: Position): Block {
 
-    var positions = listOf(Position(0, 0), Position(1, 0), Position(2, 0), Position(2,1))
+    val defaultPositions = listOf(Position(0, 2), Position(1, 0), Position(1, 1), Position(1,2))
     val rightPositions = listOf(Position(0, 1), Position(1, 1), Position(2, 1), Position(2,2))
     val leftPositions = listOf(Position(0, 0), Position(0, 1), Position(1, 1), Position(2,1))
+    val middlePosition = listOf(Position(1, 0), Position(1, 1), Position(1,2), Position(2, 0))
+    var currentPosition = defaultPositions
 
     override fun getNeededPositions(): Collection<Position> {
-        return positions.map({pos -> pos.addAxes(initialPosition)})
+        return currentPosition.map({pos -> pos.addAxes(initialPosition)})
     }
 
     override fun rotate(degree: Rotation) {
-        positions = when(degree) {
-            Rotation.RIGHT_90_DEGREE -> rightPositions
-            Rotation.LEFT_90_DEGREE -> leftPositions
+        currentPosition = when(degree) {
+            Rotation.RIGHT_90_DEGREE -> {
+                if (currentPosition == leftPositions)
+                    defaultPositions
+                else if (currentPosition == rightPositions)
+                    middlePosition
+                else
+                    rightPositions
+            }
+            Rotation.LEFT_90_DEGREE -> {
+                if (currentPosition == rightPositions)
+                    defaultPositions
+                else if (currentPosition == leftPositions)
+                    middlePosition
+                else
+                    leftPositions
+            }
         }
     }
 
---
+++ src/test/kotlin/blocks/LBlock/LBlockDoubleRotationTest.kt
@@ -0,0 +1,61 @@
+package blocks.LBlock
+
+import blocks.Block
+import blocks.DoubleRotation
+import blocks.Rotation
+import blocks.implementation.LBlock
+import movements.Position
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class LBlockDoubleRotationTest : DoubleRotation {
+
+    private lateinit var lBlock: Block
+
+    @BeforeEach
+    fun setUp() {
+        lBlock = LBlock(Position(0, 0))
+    }
+
+    @Test
+    override fun `Rotate block 90 degrees to right and then to left stays on same position`() {
+        lBlock.rotate(Rotation.RIGHT_90_DEGREE)
+        lBlock.rotate(Rotation.LEFT_90_DEGREE)
+        assertEquals(
+            listOf(Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)),
+            lBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    override fun `Rotate block 90 degrees to left and then to right stays on same position`() {
+        lBlock.rotate(Rotation.LEFT_90_DEGREE)
+        lBlock.rotate(Rotation.RIGHT_90_DEGREE)
+        assertEquals(
+            listOf(Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)),
+            lBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    override fun `Rotate block 180 degrees to left`() {
+        lBlock.rotate(Rotation.LEFT_90_DEGREE)
+        lBlock.rotate(Rotation.LEFT_90_DEGREE)
+        assertEquals(
+            listOf(Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)),
+            lBlock.getNeededPositions()
+        )
+    }
+
+    @Test
+    override fun `Rotate block 180 degrees to right`() {
+        lBlock.rotate(Rotation.RIGHT_90_DEGREE)
+        lBlock.rotate(Rotation.RIGHT_90_DEGREE)
+        assertEquals(
+            listOf(Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)),
+            lBlock.getNeededPositions()
+        )
+    }
+
+}
