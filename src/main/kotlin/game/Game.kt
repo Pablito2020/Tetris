e5c05b1aa0a461493a0bc1d060d58075a1921a7c
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-04-16 21:49:05 +0200

    refactor(game): improve naming and extract method

--- src/main/kotlin/game/Game.kt
+++ src/main/kotlin/game/Game.kt
@@ -6,20 +6,30 @@ import board.Cell
 
 internal const val GAME_COLUMNS = 10
 internal const val GAME_ROWS = 20
-internal val GAME_CELL_BUFFER = 4
+internal const val GAME_CELL_BUFFER = 4
 
-class Game(val creator: BlockCreator, val scoreCalculator: ScoreCalculator) {
+class Game(private val creator: BlockCreator, val scoreCalculator: ScoreCalculator) {
 
     private val board: Board = Board(GAME_ROWS + GAME_CELL_BUFFER, GAME_COLUMNS)
     private var block: Block? = null
 
     // TODO: Need to be tested with different block types already saved on the board
     fun getGrid(): List<List<GameCell>> {
-        val list = board.board.map { it.map { GameCell(it, false) } }
-        val newList: MutableList<MutableList<GameCell>> = (list.subList(GAME_CELL_BUFFER, list.size)).map{ it.toMutableList() }.toMutableList()
+        val result = getMutableGrid()
         if (block != null)
-            block!!.getNeededPositions().forEach { newList[it.row][it.column] = GameCell(block!!.getCell(), true) }
-        return newList
+            block!!.getNeededPositions().forEach { result[it.row][it.column] = GameCell(block!!.getCell(), true) }
+        return result
+    }
+
+    private fun getMutableGrid(): MutableList<MutableList<GameCell>> {
+        val currentBoard: List<List<Cell>> = board.board
+        return getGridFromBoard(currentBoard)
+    }
+
+    private fun getGridFromBoard(currentBoard: List<List<Cell>>): MutableList<MutableList<GameCell>> {
+        val mappedBoard = currentBoard.map { row -> row.map { cell -> GameCell(cell, false) } }
+        val result = mappedBoard.subList(GAME_CELL_BUFFER, mappedBoard.size).map { it.toMutableList() }.toMutableList()
+        return result
     }
 
     fun getNextBlock() {
