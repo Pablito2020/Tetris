113836e8b3a6132d1aa05b6a4d0ebb56b3541e6a
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-04-16 21:37:40 +0200

    refactor(block): don't violate LSP with BlockLogic class

--- src/main/kotlin/blocks/implementation/BlockLogic.kt
+++ src/main/kotlin/blocks/implementation/BlockLogic.kt
@@ -9,16 +9,16 @@ import movements.Position
 internal class BlockLogic(
     private val FORMATS_OF_BLOCK: List<List<Position>>,
     private var initialPosition: Position
-) : Block {
+) {
 
     private var currentFormatIndex = 0
 
-    override fun getNeededPositions(): Collection<Position> {
+    fun getNeededPositions(): Collection<Position> {
         val currentBlockFormat = FORMATS_OF_BLOCK[currentFormatIndex]
         return currentBlockFormat.map { position -> position.addAxes(initialPosition) }
     }
 
-    override fun rotate(degree: Rotation) {
+    fun rotate(degree: Rotation) {
         currentFormatIndex = when (degree) {
             Rotation.RIGHT_90_DEGREE -> (currentFormatIndex + 1) % FORMATS_OF_BLOCK.size
             Rotation.LEFT_90_DEGREE -> {
@@ -27,10 +27,9 @@ internal class BlockLogic(
         }
     }
 
-    override fun move(direction: Direction) {
+    fun move(direction: Direction) {
         initialPosition = initialPosition.move(direction)
     }
 
-    override fun getCell() = throw Error("Subclass needed")
 
 }
