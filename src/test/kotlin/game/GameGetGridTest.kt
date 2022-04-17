48dea91a7b66f19b022fe5e63e46c95522d3fdd2
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-04-17 09:55:24 +0200

    test(game): easier get grid test

--- src/test/kotlin/game/GameGetGridTest.kt
+++ src/test/kotlin/game/GameGetGridTest.kt
@@ -5,36 +5,38 @@ import blocks.implementation.IBlock
 import board.Cell
 import movements.Position
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import score.Points
 import score.ScoreCalculator
 
 class GameGetGridTest {
 
-    @Test
-    fun `new game returns empty grid`() {
-        val game = Game(object : BlockCreator {
-            override fun getBlock() = IBlock(Position(GAME_CELL_BUFFER, (GAME_COLUMNS / 2) - 2))
+    private lateinit var game: Game
+
+    @BeforeEach
+    fun setUp() {
+        game = Game(object : BlockCreator {
+            override fun getBlock() = IBlock(Position(0, (GAME_COLUMNS / 2) - 2))
         }, object : ScoreCalculator {
             override fun getScore(cleanedRows: Int) = Points(0)
         })
+    }
+
+    @Test
+    fun `new game returns empty grid`() {
         val expectedGrid = MutableList(GAME_ROWS) { MutableList(GAME_COLUMNS) { GameCell(Cell.EMPTY, false) } }
         assertEquals(expectedGrid, game.getGrid())
     }
 
     @Test
     fun `get grid after first get block`() {
-        val game = Game(object : BlockCreator {
-            override fun getBlock() = IBlock(Position(GAME_CELL_BUFFER, (GAME_COLUMNS / 2) - 2))
-        }, object : ScoreCalculator {
-            override fun getScore(cleanedRows: Int) = Points(0)
-        })
         game.getNextBlock()
         val expectedGrid = MutableList(GAME_ROWS) { MutableList(GAME_COLUMNS) { GameCell(Cell.EMPTY, false) } }
-        expectedGrid[GAME_CELL_BUFFER + 1][(GAME_COLUMNS / 2) - 2] = GameCell(Cell.I_BLOCK, true)
-        expectedGrid[GAME_CELL_BUFFER + 1][(GAME_COLUMNS / 2) - 1] = GameCell(Cell.I_BLOCK, true)
-        expectedGrid[GAME_CELL_BUFFER + 1][(GAME_COLUMNS / 2)] = GameCell(Cell.I_BLOCK, true)
-        expectedGrid[GAME_CELL_BUFFER + 1][(GAME_COLUMNS / 2) + 1] = GameCell(Cell.I_BLOCK, true)
+        expectedGrid[1][(GAME_COLUMNS / 2) - 2] = GameCell(Cell.I_BLOCK, true)
+        expectedGrid[1][(GAME_COLUMNS / 2) - 1] = GameCell(Cell.I_BLOCK, true)
+        expectedGrid[1][(GAME_COLUMNS / 2)] = GameCell(Cell.I_BLOCK, true)
+        expectedGrid[1][(GAME_COLUMNS / 2) + 1] = GameCell(Cell.I_BLOCK, true)
         assertEquals(expectedGrid, game.getGrid())
     }
 
